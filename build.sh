6390c9d1ff926977b553ba1abdf4d482c5bc116d
remark <about.remark@outlook.com>
2024-09-03 16:20:33 +0800

    update vercel build

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
+# 安装 Python 和 pip
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip
 
 # 安装 MkDocs 及其依赖项
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
+# 构建 MkDocs 项目
 mkdocs build -d site
