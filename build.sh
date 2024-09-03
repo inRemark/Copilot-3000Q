73a1bc84d28352e45cdcac4863114f63d9e992f3
remark <about.remark@outlook.com>
2024-09-03 16:23:25 +0800

    update vercel build

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 安装 Python 和 pip
-apt-get update
-apt-get install -y python3 python3-pip
+# apt-get update
+# apt-get install -y python3 python3-pip
 
 # 安装 MkDocs 及其依赖项
 pip3 install -r requirements.txt
