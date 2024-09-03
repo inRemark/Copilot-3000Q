2a37666b2eebd06e992bb39e03bd1471a194d871
remark <about.remark@outlook.com>
2024-09-03 16:25:02 +0800

    update vercel build

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@
 # apt-get install -y python3 python3-pip
 
 # 安装 MkDocs 及其依赖项
-pip3 install -r requirements.txt
+# pip3 install -r requirements.txt
+pip3 install mkdocs
+pip3 install mkdocs-material
+pip3 install mkdocs-static-i18n
 
 # 构建 MkDocs 项目
 mkdocs build -d site
