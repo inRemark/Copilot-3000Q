b38c89e99cddee031b2df0ae284fbac7a542357e
remark <about.remark@outlook.com>
2024-09-03 16:12:05 +0800

    add pip install

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-pip install mkdocs
-pip install mkdocs-material
-pip install mkdocs-static-i18n
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+
+# 安装 MkDocs 及其依赖项
+pip install -r requirements.txt
 
 mkdocs build -d site
